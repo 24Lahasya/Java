7f337b179ab46c5dba580aefb95300ee7cc87559
Lahasya <lahasya24@gmail.com>
2023-01-31 15:14:02 +0530

    commiyting loops demo

---
+++ java folder/src/com/demo/Loops.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class Loops {
+
+        public static void main(String[] args){
+            for (int i=10; i < 20; i = i + 1) {
+                System.out.print("value of x : " + i );
+                System.out.print("\n");
+            }
+        }
+    }
+
+
