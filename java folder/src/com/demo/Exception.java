c3d11319b8b1e7c299da2f3f7918d9aa533943f0
Lahasya <lahasya24@gmail.com>
2023-02-01 10:13:37 +0530

    Adding into git

---
+++ java folder/src/com/demo/Exception.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class Exception {
+
+        public static void main(String[] args) {
+            int[] num = {0,1,2,3,4,5,6};
+            try{
+                System.out.println(num[2]);
+                System.out.println(num[7]);
+                System.out.println(10/0);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Array length is 7");
+            } catch (ArithmeticException e){
+                System.out.println("we cannot divide a number with zero");
+            }
+            System.out.println("execution completed");
+        }
+    }
